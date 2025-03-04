ac918940dd1eb8a743d1c0e7b5cb4b5ed2da7287
Katakam Akshitha <169042771+KatakamAkshitha@users.noreply.github.com>
2025-03-04 13:33:03 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,8 @@
+defadd(a,b) :
+    return a+b
+if __name__=="__main__" :
+    res=add(5,3)
+    print(f"The sum of 2 numbers is : ",res)
+#a=int(input("Enter the val"))
+#b=int(input("Enter the val"))
+#print(a+b)
