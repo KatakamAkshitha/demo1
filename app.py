71a01c6bf567ddc5bcbeb981df1ebb7640586f11
KatakamAkshitha <katakam.akshitha05@gmail.com>
2025-03-05 19:06:41 +0530

    delete all

--- app.py
+++
@@ -1,8 +0,0 @@
-defadd(a,b) :
-    return a+b
-if __name__=="__main__" :
-    res=add(5,3)
-    print(f"The sum of 2 numbers is : ",res)
-#a=int(input("Enter the val"))
-#b=int(input("Enter the val"))
-#print(a+b)
